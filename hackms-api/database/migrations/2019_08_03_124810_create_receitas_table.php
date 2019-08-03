8da02b5e5e481cb82f7ebd9a7b0ddec2c8ac550b
Leandro <leandro.souara.web@gmail.com>
2019-08-03 09:36:28 -0400

    primeiro commit

---
+++ hackms-api/database/migrations/2019_08_03_124810_create_receitas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReceitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receitas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fonte_recurso');
+            $table->decimal('valor_previsto', 15, 2);
+            $table->decimal('valor_arrecadado', 15, 2);
+            $table->integer('mes');
+            $table->integer('ano');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receitas');
+    }
+}
---
+++ hackms-front/readme.md
@@ -0,0 +1,1 @@
+# BEIJO DO LARAS <3
